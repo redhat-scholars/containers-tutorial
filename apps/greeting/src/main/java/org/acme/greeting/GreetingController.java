bf59cdaab28e545fe700364d4abdbc710e1cdab5
Alex Soto <asotobu@gmail.com>
2021-04-29 15:50:53 +0200

    Adds Dockerless section

---
+++ apps/greeting/src/main/java/org/acme/greeting/GreetingController.java
@@ -0,0 +1,13 @@
+package org.acme.greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello World";
+    }
+}
